3b072ed6a7f90fd890d1f4c9f5c6fda8ffbd3446
machzqcq <pradeep@seleniumframework.com>
2015-05-31 20:16:01 -0400

    removed fake creds

--- test/features/steps/first_jira_api.py
+++ test/features/steps/first_jira_api.py
@@ -6,7 +6,7 @@ use_step_matcher("re")
 
 @when("I query jira")
 def step_impl(context):
-    response = requests.get('http://PCW7MCHIPOLET1.inmar.com:8083/jira/rest/api/2/search?jql=assignee=pmacharl',auth=('admin','Inmar2015'))
+    response = requests.get('http://PCW7MCH**.inmar.com:8083/jira/rest/api/2/search?jql=assignee=pmacharl',auth=('admin','<replace_password>'))
     assert response.status_code == 200
     print(response.text)
 
