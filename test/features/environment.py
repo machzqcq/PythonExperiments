637039d50e1eb393762d7bf9c0b899b07f58e0c3
machzqcq <pradeep@seleniumframework.com>
2015-05-31 21:53:45 -0400

    added browser commands scenario

--- test/features/environment.py
+++ test/features/environment.py
@@ -1,20 +1,21 @@
 from selenium import webdriver
 
 def before_all(context):
-    print("Executing before all")
+     print("Executing before all")
 
 def before_feature(context, feature):
-    print("Before feature")
+     print("Before feature\n")
 
 def before_scenario(context,scenario):
     context.browser = webdriver.Chrome()
+    context.browser.maximize_window()
 
 def after_scenario(context,scenario):
     context.browser.quit()
 
 def after_feature(context,feature):
-    print("After feature")
+     print("\nAfter feature")
 
 def after_all(context):
-    print("Executing after all")
+     print("Executing after all")
 
--- test/features/steps/first_selenium.py
+++ test/features/steps/first_selenium.py
@@ -11,6 +11,31 @@ def step_impl(context):
    assert "Selenium" in title
 
 
-@then("I print domain")
+@then("I print current url")
 def step_impl(context):
     print(context.browser.current_url)
+
+
+@step("I click ABOUT link")
+def step_impl(context):
+    context.browser.find_element_by_link_text("ABOUT").click()
+    assert "ABOUT" in context.browser.title
+
+
+@then("I click back on the browser")
+def step_impl(context):
+    context.browser.back()
+    assert "Selenium" in context.browser.title
+
+
+@step("I click forward on the browser")
+def step_impl(context):
+    context.browser.forward()
+    assert "ABOUT" in context.browser.title
+
+
+@step("I click refresh on the browser")
+def step_impl(context):
+    context.browser.refresh()
+
+
